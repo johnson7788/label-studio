18fa916509a46b9396c5c7c56d4b022c7a32e5e6
johnson <johnson@github.com>
2021-06-01 15:04:06 +0800

    2021-06-01_15:04

--- my_ml_backend/components_classifier.py
+++ my_ml_backend/components_classifier.py
@@ -18,9 +18,9 @@ class ComponentsTextClassifier(LabelStudioMLBase):
         # don't forget to initialize base class...
         super(ComponentsTextClassifier, self).__init__(**kwargs)
 
-        self.train_url = "http://192.168.50.139:5010/api/train_truncate"
+        self.train_url = "http://192.168.50.139:5015/api/train_truncate"
         # self.train_url = "http://127.0.0.1:5000/api/train_truncate"
-        self.predict_url = "http://192.168.50.139:5010/api/predict_truncate"
+        self.predict_url = "http://192.168.50.139:5015/api/predict_truncate"
         # self.predict_url = "http://127.0.0.1:5000/api/predict_truncate"
 
         # 然后从配置中收集所有key，这些key将用于从任务中提取数据并形成预测
